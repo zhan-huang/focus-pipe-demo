b3617867def62458cdea6220eff6e5328eeecd2c
Zhan Huang <z.huang2@elsevier.com>
2023-07-23 17:28:09 +0100

    refactoring

--- src/App2.js
+++ src/App2.js
@@ -1,16 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect, useRef, forwardRef} from 'react';
 
 // custom hook
 function useFocus() {
   const targetRef = useRef(null);
   const sourceRef = useRef(null);
+  const listener = useRef(null);
+
+  function clean() {
+    document.removeEventListener('keydown', listener.current);
+    sourceRef.current.focus();
+  }
 
   useEffect(() => {
-    sourceRef.current = document.activeElement;
-    const listener = document.addEventListener('keydown', e => {
-      if (e.key === 'Tab') {
+    listener.current = document.addEventListener('keydown', e => {
+      if (e.key === 'Tab' && targetRef.current) {
         if (document.activeElement === sourceRef.current && !e.shiftKey) {
           targetRef.current.focus();
           e.preventDefault();
@@ -21,30 +26,28 @@ function useFocus() {
       }
     });
 
-    return () => {
-      document.removeEventListener('keydown', listener);
-      sourceRef.current.focus();
-    }
+    return clean;
   }, []);
 
-  return targetRef;
+  return [targetRef, sourceRef, clean];
 }
 
-function Modal ({onClick}) {
-  const targetRef = useFocus();
+const Modal = forwardRef(function Modal (props, ref) {
+  console.log(ref);
   return (
     <div style={{backgroundColor: 'grey', padding: '16px', margin: '16px'}}>
-      <a ref={targetRef} className="App-link" href="" onClick={(e) => {
+      <a ref={ref} className="App-link" href="" onClick={(e) => {
         e.preventDefault();
-        onClick(e);
+        props.onClick(e);
       }}>Click me!</a><br />
       <a className="App-link" href="">Learn Vue</a>
     </div>
   );
-}
+});
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [targetRef, sourceRef, clean] = useFocus();
 
   return (
     <div className="App">
@@ -54,6 +57,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          ref={sourceRef}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -67,8 +71,9 @@ function App() {
         </a>
         <a className="App-link" href="">Do not focus me if 'click me' is visible.</a>
         <a className="App-link" href="">Do not focus me if 'click me' is visible.</a>
-        {showModal && <Modal onClick={() => {
+        {showModal && <Modal ref={targetRef} onClick={() => {
           setShowModal(false);
+          clean();
         }} />}
         <a className="App-link" href="">Lalala</a>
       </header>
